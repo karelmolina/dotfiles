0344b3bae98f687e533f045ef1eceb1a8c9a3827
Molina, Karel <karelmzuniga@gmail.com>
2023-09-06 15:46:03 -0600

    adding on_attach function to null-ls

--- nvim/lua/core/plugin_config/lsp/null-ls.lua
+++ nvim/lua/core/plugin_config/lsp/null-ls.lua
@@ -8,8 +8,10 @@ end
 local formatting = null_ls.builtins.formatting -- to setup formatters
 local diagnostics = null_ls.builtins.diagnostics -- to setup linters
 
--- to setup format on save
-local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+-- enable keybinds only for when lsp server available
+local on_attach = function (client, bufr)
+	require("core.utils.lsp").on_attach(client, bufr)
+end
 
 -- configure null_ls
 null_ls.setup({
@@ -24,7 +26,7 @@ null_ls.setup({
 			},
 		}),
 		formatting.stylua, -- lua formatter
-        formatting.erb_lint,
+		formatting.erb_lint,
 		diagnostics.eslint_d.with({
 			-- js/ts linter
 			-- only enable eslint if root has .eslintrc.js
@@ -32,29 +34,10 @@ null_ls.setup({
 				return utils.root_has_file(".eslintrc.json") -- change file extension if you use something else
 			end,
 		}),
-		diagnostics.djlint,
-        diagnostics.erb_lint,
-        diagnostics.hadolint,
-        diagnostics.rubocop,
-        diagnostics.tsc,
+		diagnostics.erb_lint,
+		diagnostics.hadolint,
+		diagnostics.rubocop,
+		diagnostics.tsc,
 	},
-	-- configure format on save
-	-- on_attach = function(current_client, bufnr)
-	--  if current_client.supports_method("textDocument/formatting") then
-	--      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-	--     vim.api.nvim_create_autocmd("BufWritePre", {
-	--         group = augroup,
-	--         buffer = bufnr,
-	--         callback = function()
-	-- vim.lsp.buf.format({
-	--  filter = function(client)
-	--     --  only use null-ls for formatting instead of lsp server
-	--     return client.name == "null-ls"
-	--  end,
-	--  bufnr = bufnr,
-	-- })
-	--         end,
-	--     })
-	--  end
-	-- end,
+	on_attach = on_attach,
 })
