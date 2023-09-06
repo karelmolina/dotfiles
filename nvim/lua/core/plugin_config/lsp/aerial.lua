f5bc63b84f90b2b77fef3aaac597f83eeb928e75
Molina, Karel <karelmzuniga@gmail.com>
2023-09-06 15:45:31 -0600

    add aerial and lspkind

---
+++ nvim/lua/core/plugin_config/lsp/aerial.lua
@@ -0,0 +1,31 @@
+-- import plugin safely
+local setup, aerial = pcall(require, "aerial")
+if not setup then
+	return
+end
+
+aerial.setup({
+      attach_mode = "global",
+      backends = { "lsp", "treesitter", "markdown", "man" },
+      disable_max_lines = vim.g.max_file.lines,
+      disable_max_size = vim.g.max_file.size,
+      layout = { min_width = 28 },
+      show_guides = true,
+      filter_kind = false,
+      guides = {
+        mid_item = "├ ",
+        last_item = "└ ",
+        nested_top = "│ ",
+        whitespace = "  ",
+      },
+      keymaps = {
+        ["[y"] = "actions.prev",
+        ["]y"] = "actions.next",
+        ["[Y"] = "actions.prev_up",
+        ["]Y"] = "actions.next_up",
+        ["{"] = false,
+        ["}"] = false,
+        ["[["] = false,
+        ["]]"] = false,
+      },
+})
