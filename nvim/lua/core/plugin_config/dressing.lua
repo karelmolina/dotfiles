3e82e678597f87bce4fa4fb346082a256ffba8f4
Molina, Karel <karelmzuniga@gmail.com>
2023-09-06 15:41:44 -0600

    add dressing for better ui

---
+++ nvim/lua/core/plugin_config/dressing.lua
@@ -0,0 +1,10 @@
+-- import plugin safely
+local setup, dressing = pcall(require, "dressing")
+if not setup then
+	return
+end
+
+dressing.setup({
+		input = { default_prompt = "➤ " },
+		select = { backend = { "telescope", "builtin" } },
+})
