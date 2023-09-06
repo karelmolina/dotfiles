a9b4fdd902033a65ce29c391452ce4bba5b0481a
Molina, Karel <karelmzuniga@gmail.com>
2023-09-06 15:40:21 -0600

    create autocmd for some ui utilities
    reused from Astrovim

---
+++ nvim/lua/core/autocmds.lua
@@ -0,0 +1,43 @@
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
+local cmd = vim.api.nvim_create_user_command
+
+local utils = require("core.utils");
+
+autocmd({ "VimEnter", "FileType", "BufEnter", "WinEnter" }, {
+  desc = "URL Highlighting",
+  group = augroup("highlighturl", { clear = true }),
+  callback = function() utils.set_url_match() end,
+})
+
+autocmd("TextYankPost", {
+  desc = "Highlight yanked text",
+  group = augroup("highlightyank", { clear = true }),
+  pattern = "*",
+  callback = function() vim.highlight.on_yank() end,
+})
+
+autocmd("BufWinEnter", {
+  desc = "Make q close help, man, quickfix, dap floats",
+  group = augroup("q_close_windows", { clear = true }),
+  callback = function(args)
+    local buftype = vim.api.nvim_get_option_value("buftype", { buf = args.buf })
+    if vim.tbl_contains({ "help", "nofile", "quickfix" }, buftype) and vim.fn.maparg("q", "n") == "" then
+      vim.keymap.set("n", "q", "<cmd>close<cr>", {
+        desc = "Close window",
+        buffer = args.buf,
+        silent = true,
+        nowait = true,
+      })
+    end
+  end,
+})
+
+cmd(
+  "UpdateAllPackages",
+  function()
+    require("lazy").sync { wait = true }
+    require("astronvim.utils.mason").update_all()
+  end,
+  { desc = "Update Plugins and Mason" }
+)
