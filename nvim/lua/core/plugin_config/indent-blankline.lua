f71aeecb9f50a0c8157d3537aac411240c024c9a
Molina, Karel <karelmzuniga@gmail.com>
2024-07-31 09:56:10 -0600

    udpate indent-blankline from updated version

--- nvim/lua/core/plugin_config/indent-blankline.lua
+++ nvim/lua/core/plugin_config/indent-blankline.lua
@@ -1,7 +1,7 @@
 local status, indent = pcall(require, "ibl")
 
 if not status then
-	return
+    return
 end
 
 vim.opt.list = true
@@ -14,57 +14,9 @@ local highlight = {
 }
 
 indent.setup {
-    indent = { highlight = highlight, char = "" },
+    indent = { highlight = highlight, char = "┊" },
     whitespace = {
         highlight = highlight,
         remove_blankline_trail = false,
     },
-    scope = { enabled = false },
 }
-
--- indent.setup({
--- 	buftype_exclude = {
--- 		"nofile",
--- 		"terminal",
--- 	},
--- 	filetype_exclude = {
--- 		"help",
--- 		"startify",
--- 		"aerial",
--- 		"alpha",
--- 		"dashboard",
--- 		"lazy",
--- 		"neogitstatus",
--- 		"NvimTree",
--- 		"neo-tree",
--- 		"Trouble",
--- 	},
--- 	context_patterns = {
--- 		"class",
--- 		"return",
--- 		"function",
--- 		"method",
--- 		"^if",
--- 		"^while",
--- 		"jsx_element",
--- 		"^for",
--- 		"^object",
--- 		"^table",
--- 		"block",
--- 		"arguments",
--- 		"if_statement",
--- 		"else_clause",
--- 		"jsx_element",
--- 		"jsx_self_closing_element",
--- 		"try_statement",
--- 		"catch_clause",
--- 		"import_statement",
--- 		"operation_type",
--- 	},
--- 	show_trailing_blankline_indent = false,
--- 	use_treesitter = true,
--- 	char = "▏",
--- 	context_char = "▏",
--- 	show_current_context = true,
--- 	show_current_context_start = true,
--- })
