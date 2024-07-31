18e432b04d6af61889175436dfa79395fe912b9c
Molina, Karel <karelmzuniga@gmail.com>
2024-07-31 09:56:40 -0600

    fix lint in null-ls config

--- nvim/lua/core/plugin_config/lsp/null-ls.lua
+++ nvim/lua/core/plugin_config/lsp/null-ls.lua
@@ -1,7 +1,7 @@
 -- import null-ls plugin safely
 local setup, null_ls = pcall(require, "null-ls")
 if not setup then
-	return
+ return
 end
 
 -- for conciseness
@@ -10,37 +10,37 @@ local diagnostics = null_ls.builtins.diagnostics -- to setup linters
 
 -- enable keybinds only for when lsp server available
 local on_attach = function (client, bufr)
-	require("core.utils.lsp").on_attach(client, bufr)
+ require("core.utils.lsp").on_attach(client, bufr)
 end
 
 -- configure null_ls
 null_ls.setup({
-	-- setup formatters & linters
-	sources = {
-		--  to disable file types use
-		--  "formatting.prettier.with({disabled_filetypes = {}})" (see null-ls docs)
-		-- js/ts formatter
-		formatting.prettierd.with({
-			env = {
-				PRETTIERD_DEFAULT_CONFIG = vim.fn.expand("~/.prettierrc"),
-			},
-		}),
-		formatting.stylua, -- lua formatter
-		formatting.erb_lint,
-		diagnostics.eslint_d.with({
-			-- js/ts linter
-			-- only enable eslint if root has .eslintrc.js
-			condition = function(utils)
-				return utils.root_has_file(".eslintrc.json") -- change file extension if you use something else
-			end,
-		}),
-		diagnostics.erb_lint,
-		diagnostics.hadolint,
-		diagnostics.rubocop,
-		diagnostics.tsc,
-		null_ls.builtins.formatting.gofumpt,
-		null_ls.builtins.formatting.goimports_reviser,
-		null_ls.builtins.formatting.golines,
-	},
-	on_attach = on_attach,
+ -- setup formatters & linters
+ sources = {
+  --  to disable file types use
+  --  "formatting.prettier.with({disabled_filetypes = {}})" (see null-ls docs)
+  -- js/ts formatter
+  formatting.prettierd.with({
+   env = {
+    PRETTIERD_DEFAULT_CONFIG = vim.fn.expand("~/.prettierrc"),
+   },
+  }),
+  formatting.stylua, -- lua formatter
+  formatting.erb_lint,
+  diagnostics.eslint_d.with({
+   -- js/ts linter
+   -- only enable eslint if root has .eslintrc.js
+   condition = function(utils)
+    return utils.root_has_file(".eslintrc.json") -- change file extension if you use something else
+   end,
+  }),
+  diagnostics.erb_lint,
+  diagnostics.hadolint,
+  diagnostics.rubocop,
+  diagnostics.tsc,
+  null_ls.builtins.formatting.gofumpt,
+  null_ls.builtins.formatting.goimports_reviser,
+  null_ls.builtins.formatting.golines,
+ },
+ on_attach = on_attach,
 })
