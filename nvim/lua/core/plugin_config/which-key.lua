b4c18cc8ffea49be503d4b778c93dc2d07058dee
Molina, Karel <karelmzuniga@gmail.com>
2024-08-26 10:58:24 -0600

    remove methods to register previus key mappings

--- nvim/lua/core/plugin_config/which-key.lua
+++ nvim/lua/core/plugin_config/which-key.lua
@@ -1,22 +1,20 @@
 local status, whichKey = pcall(require, "which-key")
 
 if not status then
-	return
+  return
 end
 
 vim.o.timeout = true
 vim.o.timeoutlen = 300
 
 whichKey.setup({
-	icons = {
-		group = vim.g.icons_enabled and "" or "+",
-		separator = " ",
-	},
-	disable = {
-		filetypes = {
-			"TelescopePrompt",
-		},
-	},
+  icons = {
+    group = vim.g.icons_enabled and "" or "+",
+    separator = " ",
+  },
+  disable = {
+    filetypes = {
+      "TelescopePrompt",
+    },
+  },
 })
-
-require("core.utils").which_key_register()
--- nvim/lua/core/utils/init.lua
+++ nvim/lua/core/utils/init.lua
@@ -18,56 +18,8 @@ function M.set_url_match()
 end
 
 function M.is_available(plugin)
-	local lazy_config_avail, lazy_config = pcall(require, "lazy.core.config")
-	return lazy_config_avail and lazy_config.spec.plugins[plugin] ~= nil
-end
-
-function M.which_key_register()
-	if M.which_key_queue then
-		local wk_avail, wk = pcall(require, "which-key")
-		if wk_avail then
-			for mode, registration in pairs(M.which_key_queue) do
-				wk.register(registration, { mode = mode })
-			end
-			M.which_key_queue = nil
-		end
-	end
-end
-
-function M.set_mappings(map_table)
-	for mode, mapped in pairs(map_table) do
-		for key, options in pairs(mapped) do
-			if options then
-				local cmd = options
-				local keymaps_opts = {}
-				if type(options) == "table" then
-					cmd = options[1]
-					keymaps_opts = options
-					keymaps_opts[1] = nil
-				end
-
-				if not cmd or keymaps_opts.name then
-					if not keymaps_opts.name then
-						keymaps_opts.name = keymaps_opts.desc
-					end
-					if not M.which_key_queue then
-						M.which_key_queue = {}
-					end
-					if not M.which_key_queue[mode] then
-						M.which_key_queue[mode] = {}
-					end
-
-					M.which_key_queue[mode][key] = keymaps_opts
-				else
-					vim.keymap.set(mode, key, cmd, keymaps_opts)
-				end
-			end
-		end
-	end
-
-	if package.loaded["which-key"] then
-		M.which_key_register()
-	end -- if which-key is loaded already, register
+  local lazy_config_avail, lazy_config = pcall(require, "lazy.core.config")
+  return lazy_config_avail and lazy_config.spec.plugins[plugin] ~= nil
 end
 
 return M
