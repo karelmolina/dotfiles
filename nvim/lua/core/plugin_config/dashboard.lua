2a3a79764f82459abf5534b4091da985e8e23584
Molina, Karel <karelmzuniga@gmail.com>
2024-11-07 02:46:50 -0600

    try config for dashboard

---
+++ nvim/lua/core/plugin_config/dashboard.lua
@@ -0,0 +1,41 @@
+local status, dashboard = pcall(require, "dashboard")
+if not status then
+  return
+end
+
+local asciilogo = [[
+,---,---,---,---,---,---,---,---,---,---,---,---,---,-------,
+|1/2| 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 0 | + | ' | <-    |
+|---'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-----|
+| ->| | Q | W | E | R | T | Y | U | I | O | P | ] | ^ |     |
+|-----',--',--',--',--',--',--',--',--',--',--',--',--'|    |
+| Caps | A | S | D | F | G | H | J | K | L | \ | [ | * |    |
+|----,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'---'----|
+|    | < | Z | X | C | V | B | N | M | , | . | - |          |
+|----'-,-',--'--,'---'---'---'---'---'---'-,-'---',--,------|
+| ctrl |  | alt |                          |altgr |  | ctrl |
+'------'  '-----'--------------------------'------'  '------'
+]]
+
+local logo = string.rep("\n", 8) .. asciilogo .. "\n\n"
+
+dashboard.setup({
+  theme = "hyper",
+  config = {
+    header = vim.split(logo, "\n"),
+    -- week_header = {
+    --   enable = true,
+    -- },
+    shortcut = {
+      { desc = "󰊳 Update", group = "@property", action = "Lazy update", key = "u" },
+      {
+        icon = " ",
+        icon_hl = "@variable",
+        desc = "Files",
+        group = "Label",
+        action = "Telescope find_files",
+        key = "f",
+      },
+    },
+  },
+})
