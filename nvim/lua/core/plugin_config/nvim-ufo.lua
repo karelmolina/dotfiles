f807d69d7bb45c4a939ddf0c3fb5d896250160ca
Molina, Karel <karelmzuniga@gmail.com>
2024-12-09 15:29:03 -0600

    reset nvim-ufo configs

--- nvim/lua/core/plugin_config/nvim-ufo.lua
+++ nvim/lua/core/plugin_config/nvim-ufo.lua
@@ -1,37 +1,11 @@
 local setup, ufo = pcall(require, "ufo")
 
 if not setup then
-	return
+  return
 end
 
 ufo.setup({
-	preview = {
-		mappings = {
-			scrollB = "<C-b>",
-			scrollF = "<C-f>",
-			scrollU = "<C-u>",
-			scrollD = "<C-d>",
-		},
-	},
-	provider_selector = function(_, filetype, buftype)
-		local function handleFallbackException(bufnr, err, providerName)
-			if type(err) == "string" and err:match("UfoFallbackException") then
-				return require("ufo").getFolds(bufnr, providerName)
-			else
-				return require("promise").reject(err)
-			end
-		end
-
-		return (filetype == "" or buftype == "nofile") and "indent" -- only use indent until a file is opened
-			or function(bufnr)
-				return require("ufo")
-					.getFolds(bufnr, "lsp")
-					:catch(function(err)
-						return handleFallbackException(bufnr, err, "treesitter")
-					end)
-					:catch(function(err)
-						return handleFallbackException(bufnr, err, "indent")
-					end)
-			end
-	end,
+  provider_selector = function(bufnr, filetype, buftype)
+    return { "lsp", "indent" }
+  end,
 })
