cc498840ceb22d7da5135f5fb21a57183eb307e8
Molina, Karel <karelmzuniga@gmail.com>
2025-04-08 17:29:40 -0600

    refactor lspconfig

--- nvim/lua/core/plugin_config/lsp/lspconfig.lua
+++ nvim/lua/core/plugin_config/lsp/lspconfig.lua
@@ -1,119 +1,83 @@
--- import lspconfig plugin safely
+-- Import lspconfig and other necessary plugins safely
 local lspconfig_status, lspconfig = pcall(require, "lspconfig")
-if not lspconfig_status then
-    return
-end
+if not lspconfig_status then return end
 
--- import cmp-nvim-lsp plugin safely
 local cmp_nvim_lsp_status, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
-if not cmp_nvim_lsp_status then
-    return
-end
-
+if not cmp_nvim_lsp_status then return end
 
 local util_status, util = pcall(require, "lspconfig/util")
-if not util_status then
-    return
-end
+if not util_status then return end
 
--- enable keybinds only for when lsp server available
-local on_attach = function (client, bufr)
-    require("core.utils.lsp").on_attach(client, bufr)
-end
-
--- used to enable autocompletion (assign to every lsp server config)
+-- Enable autocompletion and set up capabilities for every LSP server
 local capabilities = cmp_nvim_lsp.default_capabilities()
 
--- Change the Diagnostic symbols in the sign column (gutter)
--- (not in youtube nvim video)
+-- Change the diagnostic symbols in the sign column (gutter)
 local signs = { Error = " ", Warn = " ", Hint = "ﴞ ", Info = " " }
 for type, icon in pairs(signs) do
     local hl = "DiagnosticSign" .. type
     vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
 end
 
--- configure html server
-lspconfig["html"].setup({
-    capabilities = capabilities,
-    on_attach = on_attach,
-})
+-- Shared on_attach function (can be reused for all servers)
+local on_attach = function(client, bufnr)
+    require("core.utils.lsp").on_attach(client, bufnr)
+end
 
--- configure css server
-lspconfig["cssls"].setup({
-    capabilities = capabilities,
-    on_attach = on_attach,
-})
-
--- configure emmet language server
-lspconfig["emmet_ls"].setup({
-    capabilities = capabilities,
-    on_attach = on_attach,
-    filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less", "svelte" },
-})
-
--- configure lua server (with special settings)
-lspconfig["lua_ls"].setup({
-    capabilities = capabilities,
-    on_attach = on_attach,
-    settings = { -- custom settings for lua
-        Lua = {
-            -- make the language server recognize "vim" global
-            diagnostics = {
-                globals = { "vim" },
-            },
-            workspace = {
-                -- make language server aware of runtime files
-                library = {
-                    [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-                    [vim.fn.stdpath("config") .. "/lua"] = true,
+-- List of LSP servers to configure
+local lsp_servers = {
+    html = {},
+    cssls = {},
+    emmet_ls = {
+        filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less", "svelte" },
+    },
+    lua_ls = {
+        settings = {
+            Lua = {
+                diagnostics = {
+                    globals = { "vim" },
+                },
+                workspace = {
+                    library = {
+                        [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+                        [vim.fn.stdpath("config") .. "/lua"] = true,
+                    },
                 },
             },
         },
     },
-})
-
-lspconfig["ts_ls"].setup({
-    capabilities = capabilities,
-    on_attach = on_attach,
-})
-
-lspconfig["yamlls"].setup({
-    capabilities = capabilities,
-    on_attach = on_attach,
-})
-
-lspconfig["pyright"].setup({
-    capabilities = capabilities,
-    on_attach = on_attach,
-})
-
-lspconfig["jsonls"].setup({
-    capabilities = capabilities,
-    on_attach = on_attach,
-})
-
-lspconfig["bashls"].setup({
-    default_config = {
-        cmd = {"bash-language-server", "start"};
-        filetypes = {"sh"};
+    ts_ls = {},
+    yamlls = {},
+    pyright = {},
+    jsonls = {},
+    bashls = {
+        default_config = {
+            cmd = { "bash-language-server", "start" },
+            filetypes = { "sh", "bash", "zsh" },  -- Added zsh for better matching
+        },
     },
-    capabilities = capabilities,
-    on_attach = on_attach,
-})
-
-lspconfig["gopls"].setup {
-  on_attach = on_attach,
-  capabilities = capabilities,
-  cmd = {"gopls"},
-  filetypes = { "go", "gomod", "gowork", "gotmpl" },
-  root_dir = util.root_pattern("go.work", "go.mod", ".git"),
-  settings = {
     gopls = {
-      completeUnimported = true,
-      usePlaceholders = true,
-      analyses = {
-        unusedparams = true,
-      },
+        cmd = { "gopls" },
+        filetypes = { "go", "gomod", "gowork", "gotmpl" },
+        root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+        settings = {
+            gopls = {
+                completeUnimported = true,
+                usePlaceholders = true,
+                analyses = {
+                    unusedparams = true,
+                },
+            },
+        },
     },
-  },
 }
+
+-- Loop through all servers and configure them
+for server, config in pairs(lsp_servers) do
+    lspconfig[server].setup({
+        capabilities = capabilities,
+        on_attach = on_attach,
+        settings = config.settings,
+        filetypes = config.filetypes,
+        default_config = config.default_config,
+    })
+end
