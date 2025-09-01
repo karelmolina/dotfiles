fbdd580369e513ac570f087168d7e58e56f0b044
Molina, Karel <karelmzuniga@gmail.com>
2025-09-01 10:02:38 -0600

    update obsidian template variables

--- nvim/lua/core/plugin_config/obsidian.lua
+++ nvim/lua/core/plugin_config/obsidian.lua
@@ -5,15 +5,53 @@ end
 
 obsidian.setup {
   workspaces = {
-    { name = "daily", path = "~/Projects/daily-tasks" },
-    -- { name = "personal", path = "~/Projects/personal" }
+    {
+      name = "daily",
+      path = "~/Projects/daily-tasks",
+    },
   },
+  ui = {
+    enable = false,
+  },
+  completion = {
+    nvim_cmp = true,
+    min_chars = 2,
+  },
+  -- disable_frontmatter = true,
+  picker = {
+    name = "telescope.nvim",
+  },
+  -- daily notes
   daily_notes = {
     folder = "daily",
-    date_format = "%Y/%M/%BB-%dd",
+    date_format = "%Y/%m/%A-%d",
     default_tags = { "daily" },
-    template = {
-      folder = "templates",
+    template = "Template, daily.md"
+  },
+  -- templates
+  templates = {
+    folder = "templates",
+    date_format = "%Y-%m-%d",
+    time_format = "%H:%M",
+    substitutions = {
+      today = function() return os.date("%A, %B %d, %Y") end,
+      yesterdayLink = function()
+        local parts = {
+          "[[daily/",
+          os.date("%Y/%m/%A-%d", os.time() - 86400),
+          "|Yesterday]]",
+        }
+        return table.concat(parts)
+      end,
+      tomorrowLink = function()
+        local parts = {
+          "[[daily/",
+          os.date("%Y/%m/%A-%d", os.time() + 86400),
+          "|Tomorrow]]",
+        }
+        return table.concat(parts)
+      end,
     }
-  }
+  },
+  wiki_link_func = "use_alias_only", -- [[Note]] -> pretty alias
 }
