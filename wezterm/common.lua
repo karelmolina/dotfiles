2cffe54a9f5981664fd38880a72a1bb05abc63a7
Molina, Karel <karelmzuniga@gmail.com>
2026-03-17 17:38:55 -0600

    chore(wezterm): update path for windows

--- wezterm/common.lua
+++ wezterm/common.lua
@@ -1,6 +1,14 @@
 local M = {}
 
-M.bg_blurred_darker = os.getenv 'HOME'
-  .. '/dotfiles/wallpapers/bg-blurred-darker.png'
+-- Detect OS (Windows uses backslash in package.config)
+local is_windows = package.config:sub(1, 1) == '\\'
+
+-- Get home directory based on OS
+local home = is_windows and os.getenv('USERPROFILE') or os.getenv('HOME')
+
+-- Use appropriate path separator
+local sep = is_windows and '\\' or '/'
+
+M.bg_blurred_darker = home .. sep .. 'dotfiles' .. sep .. 'wallpapers' .. sep .. 'bg-blurred-darker.png'
 
 return M
