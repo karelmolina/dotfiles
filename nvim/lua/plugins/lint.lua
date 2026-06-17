10644bb75b8faeb410cee8d567d542b78a3e6b90
Molina, Karel <karelmzuniga@gmail.com>
2026-06-17 15:28:59 -0600

    fix: lint names for js and ts

--- nvim/lua/plugins/lint.lua
+++ nvim/lua/plugins/lint.lua
@@ -38,10 +38,10 @@ return {
       }
 
       lint.linters_by_ft = {
-        javascript = { "eslint" },
-        javascriptreact = { "eslint" },
-        typescript = { "eslint" },
-        typescriptreact = { "eslint" },
+        javascript = { "eslint_d" },
+        javascriptreact = { "eslint_d" },
+        typescript = { "eslint_d" },
+        typescriptreact = { "eslint_d" },
         php = { "phpstan" },
         go = { "golangcilint" },
         dart = { "dart_analyze" },
